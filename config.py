c27da48e73af3c052f37bd69bf48eb06c48f1b9a
Yank7589 <157948000+Yank7589@users.noreply.github.com>
2024-02-05 23:11:13 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("6463385416:6561779740:AAE66YBfrZIKebrpAf_h-o5zxGFLWt31hO0", "6561779740:AAE66YBfrZIKebrpAf_h-o5zxGFLWt31hO0")
+
+    
